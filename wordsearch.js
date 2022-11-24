d920615312c9d9af8c362f6bdae28235a4eebb22
James Brown <jamesraymondbrown@gmail.com>
2022-11-24 02:51:11 +0000

    wanted to fix up formatting but it turns out VSCode makes tabs 4 spaces instead of 2 when I'm livesharing. It's too much to clean right now. Sad!

--- wordsearch.js
+++ wordsearch.js
@@ -4,22 +4,19 @@ const wordSearch = (letters, word) => {
         return false;
     }
     const horizontalJoin = letters.map(ls => ls.join(''))
-    //console.log('horizontalJoin', horizontalJoin);
     for (l of horizontalJoin) {
         if (l.includes(word)) {
             output = true;
         } 
-    } 
+    }
     let newArray = [];
     for (let i = 0; i < letters[0].length; i++) {
         for (let j = 0; j < letters.length; j++) {
             newArray.push(letters[j][i])
         }
     } 
-
     for (let i = letters.length; i < newArray.length; i+=letters.length+1) {
         newArray.splice(i, 0, " ");
-        
     }
     let verticalWords = newArray.join('');
         if (verticalWords.includes(word)) {
@@ -28,16 +25,4 @@ const wordSearch = (letters, word) => {
     return output;
 }
 
-module.exports = wordSearch
-
-console.log(wordSearch([
-    ['A', 'E', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['S', 'L', 'N', 'N', 'D', 'E', 'L', 'P'],
-    ['E', 'D', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-    ['S', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-    ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-    ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-    ['N', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-    ['F', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-  ], 'SEINFELD'))
+module.exports = wordSearch;
